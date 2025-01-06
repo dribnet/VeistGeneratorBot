f91282e64dfe4f419a4b7603721d7824ebb5f780
boylesz <63565717+boylesz@users.noreply.github.com>
2025-01-06 18:31:27 +1300

    Generate image being used as test command

--- commands/utility/generateImage.js
+++ commands/utility/generateImage.js
@@ -1,4 +1,5 @@
 const { SlashCommandBuilder, PermissionFlagsBits, ChannelType, MessageFlags } = require('discord.js');
+const VGenerator = require('../../models/VGenerator');
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -6,22 +7,11 @@ module.exports = {
         .setDescription('Generate an image using the generative_recsys API endpoint.')
         .setDefaultMemberPermissions(PermissionFlagsBits.Administrator),
     async execute(interaction) {
-        interaction.deferReply();
-
-        const { Client } = await import('@gradio/client'); // Dynamic import
-                                
-        const response_0 = await fetch("https://raw.githubusercontent.com/gradio-app/gradio/main/test/test_files/bus.png");
-        const exampleImage = await response_0.blob();
-                                
-        const client = await Client.connect("rynmurdock/generative_recsys");
-        const result = await client.predict("/choose", { 
-                        img: exampleImage, 
-        });
-
-        console.log(result.data);
-
-        interaction.editReply({
-            content: 'Image generated',
+        
+        const gen = await VGenerator.findByPk('default');
+        
+        interaction.reply({
+            content: "Generator Type: " + gen.properties.gen_type,
             flags: MessageFlags.Ephemeral
         })
     }
