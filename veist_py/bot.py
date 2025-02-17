abbb98fa056e5a3a03dfff8b8b7a13b67e94f6ab
Tom White <tom@sixdozen.com>
2025-02-17 18:57:44 +0800

    ping/pong working on discord

--- veist_py/bot.py
+++ veist_py/bot.py
@@ -13,6 +13,7 @@ class VeistBot(commands.Bot):
     def __init__(self):
         intents = discord.Intents.default()
         intents.message_content = True
+        
         super().__init__(command_prefix='!', intents=intents)
 
     async def setup_hook(self):
